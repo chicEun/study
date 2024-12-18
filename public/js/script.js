bd6723c72802c1a4ecc097469df59250aab45cca
chicEun <sjrnfl7@gmail.com>
2024-12-18 18:08:52 +0900

    feat: 버튼 클릭시 메세지 표시 기능 추가

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,12 @@ java.innerHTML = "우리 교수님 쵝오 Good~!";
 
 java.style.color = "blue";
 
+function btnClik() {
+    const btnPush = document.getElementById("btn-push");
+        if(btnPush.style.display === "none") {
+            btnPush.style.display = "block";
+            btnPush.innerHTML = "힝 속았지롱~~~";
+        }else {
+            btnPush.style.display = "none";
+        }
+}
